161c18f8790b39b22166096be7870966cbd9c01d
Сивушкова Ольга Максимовна (psheom) <psheom@maxi-net.ru>
2024-07-29 16:14:53 +0300

    Скрипт проверки порта

---
+++ scripts/port_test.py
@@ -0,0 +1,24 @@
+import socket
+import sys
+
+if len(sys.argv) != 3:
+    print("Usage: port_test.py <IP> <PORT>")
+    exit(1)
+
+print("Check port")
+
+ip = sys.argv[1]
+port = int(sys.argv[2])
+
+print("IP:\t{}".format(ip))
+print("PORT:\t{}".format(port))
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+result = sock.connect_ex((ip, port))
+
+if result == 0:
+    print("Port is open")
+else:
+    print("Port is not open")
+
+sock.close()
